ac9d362b06a197f6c98adc061484950c48b47413
Alex Sampley <alex.sampley@gmail.com>
2026-05-15 08:13:10 -0600

    Get npm dependencies with nix, switch to flake-parts to do per system logic

--- flake.nix
+++ flake.nix
@@ -6,140 +6,150 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = { self, nixpkgs, fenix }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f rec {
-        inherit system;
+  outputs = inputs@{ self, nixpkgs, fenix, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } ({ ... }: {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      perSystem = { lib, system, ... }:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.android_sdk.accept_license = true;
+            config.allowUnfree = true;
+          };
 
-        pkgs = import nixpkgs {
-          inherit system;
-          config.android_sdk.accept_license = true;
-          config.allowUnfree = true;
-        };
+          androidComposition = pkgs.androidenv.composeAndroidPackages {
+            platformVersions = [ "35" "36" ];
+            buildToolsVersions = [ "35.0.0" "36.0.0" ];
+            systemImageTypes = [ "google_apis_playstore" ];
+            abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+            includeNDK = true;
+            includeExtras = [ "extras;google;auto" ];
+          };
 
-        androidComposition = pkgs.androidenv.composeAndroidPackages {
-          platformVersions = [ "35" "36" ];
-          buildToolsVersions = [ "35.0.0" "36.0.0" ];
-          systemImageTypes = [ "google_apis_playstore" ];
-          abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
-          includeNDK = true;
-          includeExtras = [ "extras;google;auto" ];
-        };
+          rustToolchain = with fenix.packages.${system}; combine ([
+            stable.cargo
+            stable.clippy
+            stable.rustc
+            stable.rust-src
+            stable.rustfmt
+          ]
+          ++ map (
+            target:
+            fenix.packages.${system}.targets.${target}.stable.rust-std
+          ) [
+            "aarch64-linux-android"
+            "armv7-linux-androideabi"
+            "i686-linux-android"
+            "x86_64-linux-android"
+            "x86_64-unknown-linux-gnu"
+          ]);
 
-        rustToolchain = with fenix.packages.${system}; combine ([
-          stable.cargo
-          stable.clippy
-          stable.rustc
-          stable.rust-src
-          stable.rustfmt
-        ]
-        ++ map (
-          target:
-          fenix.packages.${system}.targets.${target}.stable.rust-std
-        ) [
-          "aarch64-linux-android"
-          "armv7-linux-androideabi"
-          "i686-linux-android"
-          "x86_64-linux-android"
-          "x86_64-unknown-linux-gnu"
-        ]);
+          nativeBuildInputs = with pkgs; [
+            # rust build tools
+            rustToolchain
 
-        nativeBuildInputs = with pkgs; [
-          # rust build tools
-          rustToolchain
+            # node build tools
+            nodejs
 
-          # node build tools
-          nodejs
+            # Linux build tools
+            pkg-config
+            gobject-introspection
+            webkitgtk_4_1
+            wrapGAppsHook4
 
-          # Linux build tools
-          pkg-config
-          gobject-introspection
-          webkitgtk_4_1
-          wrapGAppsHook4
+            # Android build tools
+            androidComposition.androidsdk
+            jdk_headless
 
-          # Android build tools
-          androidComposition.androidsdk
-          jdk_headless
+            # AppImage build tools
+          ];
 
-          # AppImage build tools
-        ];
-
-        buildInputs = with pkgs; [
-          at-spi2-atk
-          atkmm
-          cairo
-          gdk-pixbuf
-          glib
-          glib-networking
-          gtk3
-          harfbuzz
-          librsvg
-          libsoup_3
-          pango
-          webkitgtk_4_1
-          openssl
-        ];
-      });
-      manifest = (nixpkgs.lib.importTOML src-tauri/Cargo.toml).package;
-    in {
-      packages = forAllSystems ({ pkgs, system, nativeBuildInputs, buildInputs, ... }: {
-        web = pkgs.buildNpmPackage (finalAttrs: {
-          pname = manifest.name + "-web";
-          version = manifest.version;
-
-          npmDeps = pkgs.importNpmLock { npmRoot = ./.; };
-          npmConfigHook = pkgs.importNpmLock.npmConfigHook;
-
-          nativeBuildInputs = with pkgs.pkgsBuildHost; [
+          buildInputs = with pkgs; [
+            at-spi2-atk
+            atkmm
+            cairo
+            gdk-pixbuf
+            glib
+            glib-networking
+            gtk3
+            harfbuzz
             librsvg
+            libsoup_3
+            pango
+            webkitgtk_4_1
+            openssl
           ];
+          manifest = (lib.importTOML src-tauri/Cargo.toml).package;
+        in {
+        packages = {
+          web = pkgs.buildNpmPackage (finalAttrs: {
+            pname = manifest.name + "-web";
+            version = manifest.version;
 
-          installPhase = ''
-            cp -r build $out
-          '';
+            npmDeps = pkgs.importNpmLock {
+              npmRoot = ./.;
+            };
 
-          src = ./.;
-        });
-        #default = pkgs.rustPlatform.buildRustPackage rec {
-        #  pname = manifest.name;
-        #  version = manifest.version;
-        #  cargoLock.lockFile = ./Cargo.lock;
-        #  src = pkgs.lib.cleanSource ./.;
+            npmConfigHook = pkgs.importNpmLock.npmConfigHook;
 
-        #  inherit nativeBuildInputs buildInputs;
-        #};
-      });
+            nativeBuildInputs = with pkgs.pkgsBuildHost; [
+              librsvg
+            ];
 
-      devShells = forAllSystems (shared: {
-        default = with shared.pkgs; mkShell rec {
-          #inputsFrom = [ self.packages.${system}.default ];
+            installPhase = ''
+              cp -r build $out
+            '';
 
-          nativeBuildInputs = shared.nativeBuildInputs;
+            src = ./.;
+          });
+          #default = pkgs.rustPlatform.buildRustPackage rec {
+          #  pname = manifest.name;
+          #  version = manifest.version;
+          #  cargoLock.lockFile = ./Cargo.lock;
+          #  src = pkgs.lib.cleanSource ./.;
 
-          buildInputs = shared.buildInputs ++ [
-            clippy
-            rust-analyzer
-            rustfmt
-            svelte-language-server
-            typescript-language-server
-            jmtpfs
-          ];
-
-          WEBKIT_DISABLE_DMABUF_RENDERER = "1";
-          #XDG_DATA_DIRS = "${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS";
-          GIO_MODULE_DIR = "${shared.pkgs.glib-networking}/lib/gio/modules/";
-
-          ## Android build environment
-          ANDROID_HOME = "${shared.androidComposition.androidsdk}/libexec/android-sdk";
-          NDK_HOME = "${shared.androidComposition.androidsdk}/libexec/android-sdk/ndk/${builtins.head (shared.pkgs.lib.lists.reverseList (builtins.split "-" "${shared.androidComposition.ndk-bundle}"))}";
-          ANDROID_SDK_ROOT = "${shared.androidComposition.androidsdk}/libexec/android-sdk";
-          ANDROID_NDK_ROOT = "${shared.androidComposition.androidsdk}/libexec/android-sdk/ndk-bundle";
-
-          # override the aapt2 binary that gradle uses with the patched one from the sdk
-          GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${shared.androidComposition.androidsdk}/libexec/android-sdk/build-tools/36.0.0/aapt2";
+          #  inherit nativeBuildInputs buildInputs;
+          #};
         };
-      });
-    };
+
+        devShells = {
+          default = with pkgs; mkShell {
+            npmDeps = importNpmLock.buildNodeModules {
+              npmRoot = ./.;
+              nodejs = nodejs;
+            };
+
+            #inputsFrom = [ self.packages.${system}.default ];
+
+            nativeBuildInputs = with pkgs.pkgsBuildHost; nativeBuildInputs ++ [
+              importNpmLock.hooks.linkNodeModulesHook
+            ];
+
+            buildInputs = buildInputs ++ [
+              clippy
+              rust-analyzer
+              rustfmt
+              svelte-language-server
+              typescript-language-server
+              jmtpfs
+            ];
+
+            WEBKIT_DISABLE_DMABUF_RENDERER = "1";
+            #XDG_DATA_DIRS = "${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS";
+            GIO_MODULE_DIR = "${pkgs.glib-networking}/lib/gio/modules/";
+
+            ## Android build environment
+            ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
+            NDK_HOME = "${androidComposition.androidsdk}/libexec/android-sdk/ndk/${builtins.head (pkgs.lib.lists.reverseList (builtins.split "-" "${androidComposition.ndk-bundle}"))}";
+            ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
+            ANDROID_NDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk/ndk-bundle";
+
+            # override the aapt2 binary that gradle uses with the patched one from the sdk
+            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidComposition.androidsdk}/libexec/android-sdk/build-tools/36.0.0/aapt2";
+          };
+        };
+      };
+    });
 }
